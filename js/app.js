64fc73810f3390c0d91f4328405a11504b00be13
omarakamal <omaraakamal@gmail.com>
2025-11-25 20:00:48 +0300

    update

--- js/app.js
+++ js/app.js
@@ -21,11 +21,14 @@ const choices = ['rock','paper','scissors']
 
 let playerChoice
 let computerChoice
+let message
 
 /*------------------------ Cached Element References ------------------------*/
 
 
 const choiceButtonElements = document.querySelectorAll('.choice-button')
+
+const resultDisplayElement = document.querySelector('#result-display')
 
 console.log(choiceButtonElements)
 
@@ -34,14 +37,42 @@ for (let oneElement of choiceButtonElements){
 
 }
 
+
+function getPlayerChoice(event){
+        playerChoice = event.target.id
+
+}
+
+function getComputerChoice(){
+        computerChoice = choices[Math.floor(Math.random()*3)]
+
+}
+
+function compareChoices(){
+     if(playerChoice === computerChoice){
+        message = 'You tied'
+    }
+    else if(playerChoice === 'paper' && computerChoice === 'scissors'){
+        message = 'You Lose'
+    }
+    else if(playerChoice === 'scissors' && computerChoice === 'rock'){
+        message = 'You Lose'
+
+    }
+
+    else if(playerChoice === 'rock' && computerChoice === 'paper'){
+        message = 'You Lose'
+    }
+    else{
+       message = 'You Win'
+    }
+}
+
 function handleClick(event){
-    playerChoice = event.target.id
-
-    computerChoice = choices[Math.floor(Math.random()*3)]
-
-    console.log('player picked ' + playerChoice)
-    console.log('computer picked ' + computerChoice)
-
+    getPlayerChoice(event)
+    getComputerChoice()
+    compareChoices()
+    resultDisplayElement.textContent = 'you picked ' + playerChoice + ' Computer picked ' + computerChoice + " " +message
 }
 
 
